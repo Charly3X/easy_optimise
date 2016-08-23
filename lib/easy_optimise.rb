8cbcbb9680a1244eb63cd3c803e5f367a19e7ad3
Charly3X <kinovhod@gmail.com>
2016-08-23 08:25:02 +0300

    Update easy_optimise.rb

--- lib/easy_optimise.rb
+++ lib/easy_optimise.rb
@@ -18,7 +18,7 @@ module EasyOptimise
             attachment = self.send(name)
             if attachment.content_type =~ /^image\/(png|gif|jpeg)/
               path = attachment.queued_for_write[style].path
-              width = attachment.styles[style].geometry.split('x').first.to_i
+              width = attachment.styles[style].geometry.split('x').first
               Paperclip.run('convert', "-filter Triangle -define filter:support=2 -thumbnail #{width} -unsharp 0.25x0.25+8+0.065 -dither None -posterize 136 -quality 82 -define jpeg:fancy-upsampling=off -define png:compression-filter=5 -define png:compression-level=9 -define png:compression-strategy=1 -define png:exclude-chunk=all -interlace none -colorspace sRGB -strip #{path} #{path}")
             end
           })
